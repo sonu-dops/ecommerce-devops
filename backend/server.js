49435641e175acfd21e6cb4af02dc6bf9d6dcc8e
Sonu <sonukrdops@gamil.com>
2026-07-21 03:35:38 +0530

    Fix server

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,24 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const analyticsRoutes = require("./routes/analyticsRoutes");
 
+// Load environment variables
 dotenv.config();
+
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Database
 const pool = require("./config/db");
+
+// Routes
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const analyticsRoutes = require("./routes/analyticsRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
-
-
-const app = express();
 
 // ========================================
 // Middlewares
@@ -27,7 +30,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-
 
 // ========================================
 // Database Connection
